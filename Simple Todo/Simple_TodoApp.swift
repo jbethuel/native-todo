9a6c1392a15433f2b2a1eb80b4406a103eeca97c
Bethuel Dela Cruz <jbethueldc@gmail.com>
2023-03-21 23:09:43 +0800

    Initial Commit

---
+++ Simple Todo/Simple_TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_TodoApp.swift
+//  Simple Todo
+//
+//  Created by Bethuel Dela Cruz on 3/21/23.
+//
+
+import SwiftUI
+
+@main
+struct Simple_TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
