14665c0858e802894379004d1b5146f0c9b1db9e
Bethuel Dela Cruz <jbethueldc@gmail.com>
2023-04-10 23:20:09 +0800

    simple UI

--- Simple Todo/ContentView.swift
+++ Simple Todo/ContentView.swift
@@ -7,13 +7,43 @@
 
 import SwiftUI
 
+struct Todo: Identifiable {
+    let name: String
+    let id = UUID()
+}
+
+private var todos = [
+    Todo(name: "Wake Up"),
+    Todo(name: "Make bed"),
+    Todo(name: "Wash face and brush"),
+    Todo(name: "Cook"),
+    Todo(name: "Eat")
+]
+
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image("swift-logo")
-            Text("The quick brown fox jumps over the lazy dog").bold().padding(Edge.Set.top, 3)
+            VStack {
+                ScrollView {
+                    ForEach(todos, id: \.id) { todo in
+                        HStack {
+                            Text(todo.name)
+                            Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                            }.padding(.trailing)
+                        }
+                    }
+                }
+            }
+            
+            HStack {
+                TextField(/*@START_MENU_TOKEN@*/"Add a todo"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                Button("Submit") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+            }
+            
         }.padding()
-        
     }
 }
 
